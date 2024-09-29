af1d55e7177ef0550b4ed3abe8fe4d16aa5dd2cb
bobou <sunbonoy@gmail.com>
2024-09-29 14:19:23 +0800

    Update check_sh_cron.sh

--- check_sh_cron.sh
+++ check_sh_cron.sh
@@ -6,10 +6,10 @@ START_SCRIPT="${WORKDIR}/check-process.sh"
 CRON_JOB="0 */2 * * * nohup ${START_SCRIPT} >/dev/null 2>&1 &"
 LOG_FILE="${WORKDIR}/check.log"
 
-echo "检查 check-process.sh 文件是否存在，并添加定时任务" | tee -a "$LOG_FILE"
+echo "$(date):检查 check-process.sh 文件是否存在，并添加定时任务" | tee -a "$LOG_FILE"
 
 if [ -e "${START_SCRIPT}" ]; then
-  echo "check-process.sh 文件存在，检查 crontab 中是否有相应任务" | tee -a "$LOG_FILE"
+  echo "$(date): check-process.sh 文件存在，检查 crontab 中是否有相应任务" | tee -a "$LOG_FILE"
   if (crontab -l | grep -F "$START_SCRIPT" > /dev/null); then
     echo "$(date): 定时任务已存在，无需添加。" | tee -a "$LOG_FILE"
   else
